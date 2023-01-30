4f72191d1ffce8bdb8bf2270377b924419028846
ProstoLeksus <animator-67@yandex.ru>
2023-01-30 19:54:09 +0300

    add Car

--- main.go
+++ main.go
@@ -1,22 +1,84 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/google/uuid"
-)
+type Colour struct {
+	Name string
+	Code string
+}
+type Car struct {
+	Colours []Colour
+}
+
+// func (car *Car) GetColour() string {
+// 	return car.Colour.Name
+// }
+
+// func (car *Car) AddCar(colour string) Colour {
+// 	car.Colour = colour
+// 	return car.Colour
+// }
+
+// func (car *Car) DeleteColour() {
+// 	car.Colour = Colour{}
+// }
 
 func main() {
-	id := uuid.New()
-	array := [5]int{5, 7, 9, 2, 2}
-	target := 11
-	fmt.Println(id)
-	fmt.Println(id)
-	a := twoSum(array, target)
-	fmt.Println(a)
+	// var green Colour
+	green := Colour{Name: "green", Code: "#000000"}
+	blue := Colour{Name: "blue", Code: "#000333"}
+	var colours []Colour
+	colours = append(colours, green, blue)
+
+	var car1 Car
+	car1.Colours = colours
+	fmt.Println(car1.Colours)
+	// car1.DeleteColour()
+	// fmt.Println(car1.Colour)
+	// colour1 := colour1.DeleteColour()
+	// var car1 Car
+	// colour1 := colour1.AddColour("Blue")
+	// var car1 Car
+	// colour1 := colour1.GetColour()
+	// fmt.Println(colour1)
 }
 
-func twoSum(nums [5]int, target int) []int {
-	result := []int{2, 3, 4}
-	return result
-}
+/*
+1 - Создать СТРУКТУРУ Map
+2 - Добавить в нее поля для хранения ключей и значений
+3 - Создать метод структуры Add - метод который будет добавлять структуру новые значения
+4 - Создать метод структуры Get - метод который будет возвращать знначение по ключу
+5 - Создать метод структуры Delete - метод который будет удалять из структуры по ключу
+*/
+//  6* - реализовать  сохранение данных любого типа/  почитать про приведение типов (UpCast DownCast)
+
+// func main() {
+// 	var student1 Student
+// 	student1.Name = "Jony"
+// 	student1.GroupNumber = 4
+
+// 	var student2 Student
+// 	student2.Name = "Vasya"
+// 	student2.GroupNumber = 5
+
+// 	var student3 Student
+// 	student3.Name = "Nikita"
+// 	student3.GroupNumber = 5
+
+// 	name := student3.GetName()
+// 	fmt.Println(name)
+
+// 	id := uuid.New()
+// 	idString := id.String()
+// 	m := make(map[string]Student)
+// 	m[idString] = student1
+// 	m[uuid.New().String()] = student2
+// 	m[uuid.New().String()] = student3
+
+// 	for key, value := range m {
+// 		if value.Name == "Nikita" {
+// 			value.GroupNumber = 3
+// 			fmt.Println(key, value)
+// 		}
+// 	}
+// }
---
+++ true.go
@@ -0,0 +1,34 @@
+package main
+
+// import "fmt"
+
+// type Student struct {
+// 	Name        map[int]string
+// 	GroupNumber int
+// }
+
+// func (s *Student) GetName() string {
+// 	s.Name.//у мапы есть функция delete
+// 	return s.Name
+// }
+// func (s *Student) AddName(name string, namber int) (string, int) {
+// 	s.Name = name
+// 	s.GroupNumber = namber
+// 	return s.Name, s.GroupNumber
+// }
+// func (s *Student) DeleteName() string {
+// 	return s.Name
+// }
+
+// func main() {
+// 	var student1 Student
+// 	name1, group1 := student1.AddName("Den", 6)
+
+// 	fmt.Println(name1, group1)
+
+// 	m := make(map[int]Student)
+// 	id := 1
+// 	idString := id
+// 	m[idString] = student1
+
+// }
